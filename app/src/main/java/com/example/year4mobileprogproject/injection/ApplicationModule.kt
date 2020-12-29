41ce84ae141eb3db84fa27710bd688275b7dec3f
SilverDove <tricot.clara99@gmail.com>
2020-12-29 10:15:26 +0100

    Fix errors

--- app/src/main/java/com/example/year4mobileprogproject/injection/ApplicationModule.kt
+++ app/src/main/java/com/example/year4mobileprogproject/injection/ApplicationModule.kt
@@ -1,5 +1,8 @@
 package com.example.year4mobileprogproject.injection
 
+import com.example.year4mobileprogproject.presentation.main.MainViewModel
 import org.koin.dsl.module
 
-val presentationModule = module {  }
+val presentationModule = module {
+    factory{MainViewModel()}
+}
